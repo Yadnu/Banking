d652d2fcd61c51224dea224483fcbfe6b5659391
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-04-27 00:11:15 +0530

    Bootstraping

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
+
 export default function Page(): JSX.Element {
+  const session = useSession();
   return (
-    <div className="text-ellipsis text-2xl">
-      Hi There
-    </div>
+   <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
   );
 }
